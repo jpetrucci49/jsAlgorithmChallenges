c0465d89c76b9f38f80c454419bc4abd18941d02
jpetrucci <jpetrucci49@yahoo.com>
2018-11-25 17:04:24 -0500

    Add success check
    If the elements of expected and result are not equal, success is false

--- common/test.js
+++ common/test.js
@@ -1,6 +1,17 @@
 module.exports = function (result, expected, message) {
     let success = true;
 
+    if (Array.isArray(result) && Array.isArray(expected)) {
+        for (let i = 0; i < result.length && i < expected.length; i++) {
+            if (result[i] !== expected[i]) {
+                success = false;
+                break;
+            }
+        }
+    } else if (result !== expected) {
+        success = false;
+    }
+
     if (success) {
         console.log(`The test was successful: got ${result}`);
     } else if (message) {
